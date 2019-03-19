0a61fba6d8b57330a2b13f3b1dee5d9ac9a34d19
Nathan-E <eziokwubunduchibueze.com>
2019-03-19 14:45:10 +0100

    database connection logic created

--- models/group-message.js
+++ models/group-message.js
@@ -1,3 +1,10 @@
+const mongoose = require('mongoose');
+
+//Database connection
+mongoose.connect('mongodb://localhost/bears', { useNewUrlParser: true })
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
+
 
 
 
