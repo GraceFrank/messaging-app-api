6613de46037f4da4815dc9774dd789fdd45aff8a
Nathan-E <eziokwubunduchibueze.com>
2019-03-19 14:52:08 +0100

    groupMessage schema formed

--- models/group-message.js
+++ models/group-message.js
@@ -5,10 +5,21 @@ mongoose.connect('mongodb://localhost/bears', { useNewUrlParser: true })
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
 
+//Group-Message Schema
+const groupMessageSchema = mongoose.Schema({
+  groupId: {
+    type: String,
+    required: true
+  },
+  groupName: {
+    type: String,
+    required: true
+  },
+  groupMessage: {
+    type: Object,
+    required: true
+  }
+});
 
-
-
-
-const GroupMessage;
 
 module.exports = GroupMessage;
