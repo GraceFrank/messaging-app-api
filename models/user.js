ddf73b8c99a8493b8db7fb98134fb3efdaa18096
Tyav <tyav2greenz@gmail.com>
2019-03-19 14:47:35 +0100

    created user schema & model

--- models/user.js
+++ models/user.js
@@ -1,7 +1,39 @@
+//Schema for User
+const mongoose = require('mongoose');
 
+//Connect to data base
+mongoose.connect('mongodb://localhost/users', { useNewUrlParser: true })
+  .then(() => console.log(`Conneted to the Database...`))
+  .catch((err) => console.log(err));
 
+const userSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+		minlength: 6,
+	},
+	email: {
+		type: String,
+		required: true,
+	},
+	private_chat: {
+		type: Object,
+		default: {},
+	},
+	group_chat: {
+		type: Object,
+		default: {},
+	},
+	friend_list: {
+		type: [ String ],
+		default: [],
+	},
+});
 
-
-const User
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
