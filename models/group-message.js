24d1066645cd333e3eef229c66f45142117bb70f
Nathan-E <eziokwubunduchibueze.com>
2019-03-19 16:13:55 +0100

    messageObjectSchema created as a sub document for the groupMessageSchema

--- models/group-message.js
+++ models/group-message.js
@@ -5,6 +5,23 @@ mongoose.connect('mongodb://localhost/bears', { useNewUrlParser: true })
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
 
+//Message Object Schema (a sub document of the group message schema)
+const messageObjectSchema = mongoose.Schema({
+  content:{
+    type: String,
+    required: true,
+    minlength: 1
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
 //Group-Message Schema
 const groupMessageSchema = mongoose.Schema({
   groupId: {
