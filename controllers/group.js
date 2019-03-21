8394ee0af8d47baed1688a190fadf7d3e9745eb2
Nathan-E <eziokwubunduchibueze.com>
2019-03-21 04:15:05 +0100

    group create controller refactored to reflect changes

--- controllers/group.js
+++ controllers/group.js
@@ -6,23 +6,20 @@ const group = {}
 
 //POST 
 group.post = async (req, res) => {
-  try { 
+  try {
     //validates the input
     const { error } = validateGroup(req.body);
     //access the error message based on Joi error object
     if (error) return res.status(404).send(error.details[0].message);
+    console.log('done');
 
     //takes in the required parameter
     const group = new Group({
-      groupId: req.body.groupId,
+      authorId: req.body.authorId,
       groupName: req.body.groupName,
-      groupMessage: [{
-        content: req.body.groupMessage.content,
-        author: req.body.groupMessage.author,
-        date: req.body.groupMessage.date
-      }]
+      groupMessage: []
     });
-
+    console.log('done');
     //save the group to the database
     await group.save();
     res.send(group);
@@ -40,7 +37,7 @@ group.put = async (req, res) => {
     if (error) return res.status(404).send(error.details[0].message);
 
     //checks for the field with that matches with the id given
-    const group = await Bear.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    const group = await Group.findByIdAndUpdate(req.params.id, req.body, { new: true });
     if (!group) return res.status(400).send('The group with the given id does not exist');
 
     res.send(group);
